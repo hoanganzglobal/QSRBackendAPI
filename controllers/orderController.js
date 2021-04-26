0bed65593a9278ec8d904cf2bc0cfadd0ce74bd5
Ho Thanh Hoang (IT HCM) <hoang.t.ho@qsrvietnam.com>
2021-04-26 23:49:21 +0700

    Add Socket Order Insert

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,11 +4,37 @@ const authorize = require('../_helpers/jwt');
 
 // routes
 router.post(
+  '/notify-insert',
+  authorize(['basic']),
+  getNotifyInsert
+);
+router.post(
   '/notify-update',
-  //authorize(['basic']),
+  authorize(['basic']),
   getNotifyUpdate
 );
+
 module.exports = router;
+
+function getNotifyInsert(req, res, next) {
+  const io = req.io;
+
+  var data = {
+    orderRow: req.body.ecomOrderRow,
+  };
+
+  var orderRow = data.orderRow;
+  var payload = {
+    success: true,
+    data: orderRow[0],
+  };
+
+  req.app.get('socketService').emiter('notify order insert', payload);
+
+  payload.success === true
+    ? res.status(200).send(payload)
+    : res.status(400).json(payload);
+}
 
 function getNotifyUpdate(req, res, next) {
   const io = req.io;
